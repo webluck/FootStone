bd10b6e2c2711c2a779f79ca759f38f2516f97a0
lixl <webluck@outlook.com>
2018-01-17 23:11:54 +0800

    first button

--- src/Home.js
+++ src/Home.js
@@ -7,23 +7,45 @@ import styled, {
 class Home extends React.Component {
 
     render() {
-        const Button = styled.button `
-            border-radius: 3px;
-            padding: 0.25em 1em;
-            margin: 0 1em;
-            background: transparent;
-            color: palevioletred;
-            border: 2px solid palevioletred;
+        const Button = styled.div `
+            border:1px solid gray;
+            width:180px;
+            height:30px;
+            line-height:30px;
+            padding:5px;
+            text-align:center;
+            background-color:#0E83CD;
+            color:white;
+            position: relative;
+            :hover{
+                background-color:white;
+                cursor:pointer;
+                color:#0E83CD;
+            }
+            :active{
+                top:2px;
+            }
+            :before{
+                content:"hello-";
+            }
+            :after{
+                content:"-world";
+            }
 
-            ${props => props.primary && css`
-                background: palevioletred;
-                color: white;
-            `}
+            transition:background-color 0.3s ease-in;
+            transition:color 0.3s linear;
+
+            -webkit-touch-callout: none; /* iOS Safari */
+            -webkit-user-select: none; /* Chrome/Safari/Opera */
+            -khtml-user-select: none; /* Konqueror */
+            -moz-user-select: none; /* Firefox */
+            -ms-user-select: none; /* Internet Explorer/Edge */
+            user-select: none; /* Non-prefixed version, currently not supported by any browser */
         `;
 
         return ( 
             <div>            
-                <Button primary>Commit</Button> 
+                <Button>CssBtn</Button> 
             </div>
         )
     }
